ea82963bc63f0c50ae894d0f6899fefbe27c0b56
s22001 <s22001@std.it-college.ac.jp>
2023-08-03 17:00:03 +0900

    7.3 ライフサイクルについて理解した

--- LifeCycleSample/app/src/main/java/jp/ac/it_college/std/s22001/lifecyclesample/MainActivity.kt
+++ LifeCycleSample/app/src/main/java/jp/ac/it_college/std/s22001/lifecyclesample/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btPrevious.setOnClickListener(::onNextClick)
+        binding.btNext.setOnClickListener(::onNextClick)
     }
 
     override fun onStart() {

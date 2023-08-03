7e240f6adba47534894a24b201cf9ac6299ba0ae
s22001 <s22001@std.it-college.ac.jp>
2023-08-03 16:53:25 +0900

    7.3 ライフサイクルについて理解した

--- LifeCycleSample/app/src/main/java/jp/ac/it_college/std/s22001/lifecyclesample/MainActivity.kt
+++ LifeCycleSample/app/src/main/java/jp/ac/it_college/std/s22001/lifecyclesample/MainActivity.kt
@@ -1,11 +1,55 @@
 package jp.ac.it_college.std.s22001.lifecyclesample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import jp.ac.it_college.std.s22001.lifecyclesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btPrevious.setOnClickListener(::onNextClick)
+    }
+
+    override fun onStart() {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
+        super.onDestroy()
+    }
+
+    private fun onNextClick(view: View) {
+        val intent = Intent(this, SubActivity::class.java)
+        startActivity(intent)
     }
 }

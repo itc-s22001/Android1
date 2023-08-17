9d0119c83292569a7871ffc49302bc422012e531
s22001 <s22001@std.it-college.ac.jp>
2023-08-17 16:25:58 +0900

    10,1まで。最初の画面の表示まで出来た

--- DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22001/databasesample/MainActivity.kt
+++ DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22001/databasesample/MainActivity.kt
@@ -2,10 +2,33 @@ package jp.ac.it_college.std.s22001.databasesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jp.ac.it_college.std.s22001.databasesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // RecyclerView の初期化
+        initList(binding.lvCocktail)
+    }
+
+    private fun initList(list: RecyclerView) {
+        val data = resources.getStringArray(R.array.lv_cocktail_list)
+        val adapter = CocktailAdapter(data.toList()) { pos, name ->
+            // 選択されたカクテル名を画面上に表示
+            binding.tvCocktailName.text = name
+        }
+        val manager = LinearLayoutManager(this)
+        list.adapter = adapter
+        list.layoutManager = manager
+        list.addItemDecoration(
+            DividerItemDecoration(this, manager.orientation)
+        )
     }
 }
